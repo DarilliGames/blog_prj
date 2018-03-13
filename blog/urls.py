dbde1a9311ff515bc7832050c28296f3294ae140
Stephen Moody <stephengmoody@hotmail.com>
2018-03-13 13:43:39 +0000

    added a model for Blog Post called Post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^$', get_index),
+    url(r'^$', get_main, name="blog"),
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def get_index(request):
-    return render(request, "home/index.html")
+def get_main(request):
+    return render(request, "blog/index.html")
